2eed36871af22ad35d72b222b5430e71741321f6
rushabhkoradia <rushabhhkoradia@gmail.com>
2021-09-24 09:52:34 +0530

    Created required Schema's

---
+++ server/database/menu/index.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const MenuSchema = new mongoose.Schema({
+    menus: [{
+        name: {type: String, required: true},
+        items: [{
+            type: mongoose.Types.ObjectId,
+            ref: "Foods"
+        }]
+    }],
+    recommend: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Foods",
+        unique: true
+    }]
+},
+{
+    timestamps: true
+});
+
+export const MenuModel = mongoose.model("Menus", MenuSchema);
