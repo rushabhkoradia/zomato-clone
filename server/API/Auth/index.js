98084d2a453d7050f2e4cdaf5f82fa50b1721cbb
rushabhkoradia <rushabhhkoradia@gmail.com>
2021-09-26 18:50:49 +0530

    Created Zomato API

---
+++ server/API/Auth/index.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+const Router = express.Router();
+
+// Models
+import { UserModel }  from '../../database/user';
+
+/*
+|==============================================|
+| Route       | /signup                        |
+| Description | Signup with email and password |
+| Params      | None                           |
+| Access      | Public                         |
+| Method      | Post                           |
+|===============================================
+*/
+
+Router.post("/signup", async(req, res) => {
+    try {
+        const {email, password, fullName, phoneNumber} = req.body.credentials;
+
+        //Check whether email / phone number exists
+        const checkUserByEmail = await UserModel.findOne({ email });
+        const checkUserByPhone = await UserModel.findOne({ phoneNumber });
+
+        if (checkUserByEmail || checkUserByPhone) {
+            return res.json({ eroor: "User already exists"});
+        }
+
+        // hashing & salting (1 time excryption = hashing)
+        const bcryptSalt = await bcrypt.genSalt(8);
+        const hashedPassword = await bcrypt.hash(password, bcryptSalt);
+
+        // Database
+        await UserModel.create({
+            ...req.body.credenials,
+            password: hashedPassword
+        });
+
+        // JWT Auth Token
+        const token = jwt.sign({user: {fullName, email}}, "ZomatoApp");
+
+        return res.status(200).json({token});
+    }
+    
+    catch (error) {
+        return res.status(500).json({error: error.message});        
+    }
+});
+
+export default Router;
