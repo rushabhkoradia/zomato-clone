3596ee013f0d9dd34e1029501197545cb7f2286b
rushabhkoradia <rushabhhkoradia@gmail.com>
2021-10-14 22:59:22 +0530

    Completed Redux Basic Setup

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import { Provider } from 'react-redux';
+import Store from './Redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={ Store }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
